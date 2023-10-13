77b2576dfd32902bf67e3a15e3c76a467647be95
Arthuro <jope.zareno.up@phinmaed.com>
2023-10-13 06:22:46 -0700

    10 percent skeletal palangs

--- app/src/main/java/com/example/a299ecom/fragment/categories/MainCategoryfragment.kt
+++ app/src/main/java/com/example/a299ecom/fragment/categories/MainCategoryfragment.kt
@@ -3,5 +3,5 @@ package com.example.a299ecom.fragment.categories
 import androidx.fragment.app.Fragment
 import com.example.a299ecom.R
 
-class MainCategory: Fragment(R.layout.fragment_main_category) {
+class MainCategoryfragment: Fragment(R.layout.fragment_main_category) {
 }
--- app/src/main/java/com/example/a299ecom/fragment/shopping/HomeFragment.kt
+++ app/src/main/java/com/example/a299ecom/fragment/shopping/HomeFragment.kt
@@ -6,10 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.a299ecom.R
-import com.example.a299ecom.addapters.HomeViewPagerAddapter
+import com.example.a299ecom.addapters.HomeViewPagerAdapter
 import com.example.a299ecom.databinding.FragmentHomeBinding
 import com.example.a299ecom.fragment.categories.KidsFragment
-import com.example.a299ecom.fragment.categories.MainCategory
+import com.example.a299ecom.fragment.categories.MainCategoryfragment
 import com.example.a299ecom.fragment.categories.MenFragment
 import com.example.a299ecom.fragment.categories.WomenFragment
 import com.google.android.material.tabs.TabLayoutMediator
@@ -29,14 +29,14 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val categoriesFragments = arrayListOf<Fragment>(
-            MainCategory(),
+        val categoriesFragment = arrayListOf<Fragment>(
+            MainCategoryfragment(),
             MenFragment(),
             WomenFragment(),
             KidsFragment()
         )
 
-        val viewPager2Adapter = HomeViewPagerAddapter(categoriesFragments,childFragmentManager,lifecycle)
+        val viewPager2Adapter = HomeViewPagerAdapter(categoriesFragment,childFragmentManager,lifecycle)
         binding.viewpagerhome.adapter = viewPager2Adapter
         TabLayoutMediator(binding.tabLayout, binding.viewpagerhome) { tab, position ->
             when (position) {
